e86b7ebfeb8c6ee1f11962dea42ed2e6a38c00bc
MiguelMarcal <75666756+MiguelMarcal@users.noreply.github.com>
2023-10-27 09:39:44 +0100

    main.c
    Added the functionality of not seing the password while typing, minos changes in the code for quality of life. Still having problems storing and retrieving the passwords

--- Cfile/main.c
+++ Cfile/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <readline/readline.h>
+#include <readline/history.h>  // Include history support
+#include <termios.h>  // Include termios for terminal configuration
+#include <unistd.h>
 
+#define HASHED_KEY "9917f777c07b978d862e33c6638b132710c6ef1dcab6b8174fa50e476e316e5a"
 #define FILENAME "data.txt"
 #define MAX_STR_LEN 128
 #define SALT_SIZE 16
@@ -32,50 +38,84 @@ void encryptDecrypt(char *input, char *salt, char *password, int do_encrypt) {
 
     EVP_CIPHER_CTX_free(ctx);
 }
-int main() {
-    char password[MAX_STR_LEN];
-    char salt[SALT_SIZE];
+// Function to disable terminal echoing
+void disableEcho() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
 
-    printf("Enter the password: ");
-    fgets(password, sizeof(password), stdin);
+// Function to enable terminal echoing
+void enableEcho() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+int main() {
+    char *password = NULL;
+    char salt[MAX_STR_LEN];
+
+    disableEcho();  // Disable echoing for password input
+    password = readline("Enter the password: ");
+    enableEcho();  // Enable echoing after password input
+    printf("\n");  // Move to a new line after password input
+
+    if (password == NULL) {
+        perror("readline");
+        return 1;
+    }
 
     printf("Enter the salt (16 bytes, e.g., a random hexadecimal string): ");
     fgets(salt, sizeof(salt), stdin);
-
-    // Remove newline characters from the input
-    password[strcspn(password, "\n")] = '\0';
     salt[strcspn(salt, "\n")] = '\0';
 
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((const unsigned char *)password, strlen(password), hash);
+
+    char hashed_key[2 * SHA256_DIGEST_LENGTH + 1];
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        sprintf(&hashed_key[i * 2], "%02x", hash[i]);
+    }
+    printf("%s", hashed_key);
+    if (strcmp(hashed_key, HASHED_KEY) != 0) {
+        printf("Access Denied.\n");
+        return 0;
+    }
     while (1) {
         char option;
         printf("Do you want to:\nRead from the file   ->(R) \nWrite to the file   ->(W)\nQuit   ->(Q) ");
         scanf(" %c", &option);
 
-
         if (option == 'R' || option == 'r') {
-            // Read and decrypt strings from the file
+            clearTerminal();
+
             char line[MAX_STR_LEN];
             FILE *file = fopen(FILENAME, "a+");
 
-            clearTerminal();
 
             while (fgets(line, sizeof(line), file)) {
-                line[strcspn(line, "\n")] = '\0'; // Remove newline character
-                encryptDecrypt(line, salt, password, 0); // Decrypt the string
+                line[strcspn(line, "\n")] = '\0';
+                if(strlen(line) <=0){
+                    fclose(file);
+                    continue;
+                }
+                encryptDecrypt(line, salt, password, 0);
                 printf("Decrypted: %s\n", line);
             }
             fclose(file);
 
         } else if (option == 'W' || option == 'w') {
+            clearTerminal();
             FILE *file = fopen(FILENAME, "a+");
 
-            // Write encrypted strings to the file
-            clearTerminal();
             char new_string[MAX_STR_LEN];
             printf("Enter the string to write: ");
             scanf(" %[^\n]", new_string);
 
-            encryptDecrypt(new_string, salt, password, 1); // Encrypt the new string
+            encryptDecrypt(new_string, salt, password, 1);
             fprintf(file, "%s\n", new_string);
             printf("String added to the file.\n");
             fclose(file);
@@ -86,7 +126,6 @@ int main() {
         } else {
             printf("Invalid option.\n");
         }
-
     }
 
     return 0;
